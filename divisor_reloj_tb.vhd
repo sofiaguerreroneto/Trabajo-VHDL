76ce366e6cc14d07a4b431ac53c2dcd6bf5a50db
sofiaguerreroneto <47852302+sofiaguerreroneto@users.noreply.github.com>
2020-12-31 12:07:18 +0100

    Update divisor_reloj_tb.vhd

--- divisor_reloj_tb.vhd
+++ divisor_reloj_tb.vhd
@@ -5,21 +5,21 @@ use ieee.numeric_std.all;
 entity divisor_reloj_tb is
 end divisor_reloj_tb;
 architecture behavioral of divisor_reloj_tb is
-component divisor_reloj is  --Estructura del divisor de reolj
-	port(	clk: in std_logic;  --Entrada reloj original
-	        reset: in std_logic;  --Entrada reset
-			clk_dividido: out std_logic);  --Salida reloj dividido
+component divisor_reloj is  -- Estructura del divisor de reolj
+	port(	clk: in std_logic;  -- Entrada reloj original
+	        reset: in std_logic;  -- Entrada reset
+			clk_dividido: out std_logic);  -- Salida reloj dividido
 end component;
---Una señal para cada puerto del componente
-signal clk_s: std_logic :='1';  --Señal clock se inicializa en 0
-signal reset_s: std_logic :='0';  --Señal reset se inicializa en 0
-signal clk_dividido_s: std_logic;  --Señal reloj dividido
+-- Una seÃ±al para cada puerto del componente
+signal clk_s: std_logic :='1';  -- SeÃ±al clock se inicializa en 0
+signal reset_s: std_logic :='0';  -- SeÃ±al reset se inicializa en 0
+signal clk_dividido_s: std_logic;  -- SeÃ±al reloj dividido
 begin
-  uut: divisor_reloj port map(  --Asignación de puertos a las señales correspondientes
-	clk => clk_s,  --A la entrada clk se le asigna la señal de reloj clk_s
-	reset => reset_s,  --A la entrada reset se le asigna la señal reset_s
-	clk_dividido => clk_dividido_s);  --A la salida clk_dividido se le asigna la señal clk_dividido_s
-  --Generación de estímulos
+  uut: divisor_reloj port map(  -- AsignaciÃ³n de puertos a las seÃ±ales correspondientes
+	clk => clk_s,  -- A la entrada clk se le asigna la seÃ±al de reloj clk_s
+	reset => reset_s,  -- A la entrada reset se le asigna la seÃ±al reset_s
+	clk_dividido => clk_dividido_s);  -- A la salida clk_dividido se le asigna la seÃ±al clk_dividido_s
+  -- GeneraciÃ³n de estÃ­mulos
     reset_s <='0', '1' after 600 ns;
     clk_s <= not clk_s after 10 ns;    
 end behavioral;
